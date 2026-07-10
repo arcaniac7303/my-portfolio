17c38e1a22f7ed25932f588f6f36bc93189d33af
taetae <arcaniac142857@gmail.com>
2026-07-10 18:21:56 +0900

    .

---
+++ asset/js/script.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
